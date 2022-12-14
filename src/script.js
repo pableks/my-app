3b63abbd63c24485da5be5a273d98dac9230a34c
pableks <pablojavier.vl888@gmail.com>
2022-12-14 09:04:54 -0300

    Some descriptive commit message

--- src/script.js
+++ src/script.js
@@ -66,7 +66,7 @@ scene.add(sunLight)
 
 /////////////////////////////////////////////////////////////////////////
 ///// LOADING GLB/GLTF MODEL FROM BLENDER
-loader.load('models/gltf/pudu.glb', function (gltf) {
+loader.load('models/gltf/capybara.glb', function (gltf) {
 
     scene.add(gltf.scene)
 })
